4fa16d70b97ed40e4cae344326ec1a2adc40b872
Maxim Grinin <m.grinin@vkteam.ru>
2025-03-31 02:52:02 +0300

    config correction

--- src/config.py
+++ src/config.py
@@ -8,7 +8,8 @@ DB_PASS = os.getenv("DB_PASS")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
+SECRET = os.getenv("SECRET")
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-SECRET = 'NOT_A_SECRET'
+
